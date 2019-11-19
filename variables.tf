6c42104881f51a9b3c8582acfe2a22ed2ee81b3a
Alejandro Inestal <alejandro@seatfrog.com>
2019-11-19 11:07:10 +0000

    Fix syntax problems to work with terraform 0.12.x

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "groups" {
   description = "Enforce MFA for the members in these groups"
-  type = "list"
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "users" {
   description = "Enforce MFA for these users"
-  type = "list"
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "allow_password_change_without_mfa" {
   description = "Allow changing the user password without MFA"
-  type = "string"
-  default = "false"
+  type        = string
+  default     = "false"
 }
